b6067e04478dd20e77891746a1f54ec489e9178d
Law <lkuoch@users.noreply.github.com>
2023-12-20 00:09:29 +1100

    feat: add pong

--- app/server.go
+++ app/server.go
@@ -6,18 +6,55 @@ import (
 	"os"
 )
 
+const (
+	listenAddr = "0.0.0.0:6379"
+)
+
+func writeResponse(c net.Conn, msg string) {
+	_, err := c.Write([]byte(msg))
+
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+	}
+}
+
+func parseConnections(c net.Conn) {
+	defer c.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := c.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	request := string(buffer[:n])
+	fmt.Println("Recieved request: ", request)
+
+	// If request is `PING` respond with PONG
+	if request == "PING\r\n" {
+		writeResponse(c, "+PONG\r\n")
+	} else {
+		writeResponse(c, "???\r\n")
+	}
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go parseConnections(c)
 	}
 }
